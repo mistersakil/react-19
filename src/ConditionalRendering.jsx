d0db73a3ed414ae32c1b9b8dbcb8ec657a46e07a
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-07 17:05:32 +0600

    contional exception handling

--- src/ConditionalRendering.jsx
+++ src/ConditionalRendering.jsx
@@ -6,9 +6,12 @@ let ConditionalRendering = () => {
   return (
     <>
     <h2>Conditional rendering</h2> <hr />
-    
+    <p> if condition returns  zero '0' and empty string '' , it throws an exception and return 0, '' as an output. see example below...</p>
     <p>
-        {students.length && 'student found'}
+        condition 1: {students.length  && 'student found'}
+        <br />
+        condition 2 (solution): {students.length !=0 && 'student found'}
+        { null && 'students found'}
     </p>
     <p>number of student: {students.length} </p>
     </>
