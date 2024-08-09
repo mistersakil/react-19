8ed2bef1ff4be18a0601aac187de1fc2f5277585
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-09 20:29:10 +0600

    use of derived state or computed property

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -6,11 +6,41 @@ const State = () => {
   let handleCount = () => {
     setCount(() => count + 1);
   };
+
+  let defaultUsers = [
+    { name: "sakil", age: 30 },
+    { name: "rasel", age: 40 },
+    { name: "monir", age: 50 },
+  ];
+
+  setInterval(() => {
+    setUsers(() => {
+      return [...users, { name: "jalil", age: 45 }];
+    });
+  }, 5000);
+  let [users, setUsers] = useState(defaultUsers);
+
+  const avgAge =
+    users.reduce((total, user) => {
+      return total + user.age;
+    }, 0) / users.length;
   return (
     <>
       <h1>State</h1>
       <hr />
       <button onClick={handleCount}>count ({count})</button>
+
+      <h2>users using state</h2>
+
+      {users.map((user, index) => {
+        return <li key={index}>{user.name}</li>;
+      })}
+
+      <h2>without derived state</h2>
+      <h4>total users: {users.length}</h4>
+
+      <h2>with derived state or computed property </h2>
+      <h4>avg age: {avgAge}</h4>
     </>
   );
 };
