a47092380ea4646a2221aca7a41015b4bfcbdf6b
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-11 12:30:06 +0600

    lifting state up done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import ConditionalRendering from "./ConditionalRendering";
 // import Loops from "./components/Loops.jsx";
 // import Event from "./components/Event.jsx";
-import State from "./components/State";
+// import State from "./components/State";
+import LiftingStateUp from "./components/LiftingStateUp";
 function App() {
   return (
     <div className="App">
+      <LiftingStateUp />
       {/* <State /> */}
       {/* <Event /> */}
       {/* <Loops /> */}
--- src/components/LiftingStateUp.jsx
+++ src/components/LiftingStateUp.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LiftingStateUp = () => {
+  let [inputValue, setInputValue] = useState("");
   return (
     <>
       <h1>LiftingStateUp</h1>
+      <InputComponent
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        type="text"
+        label="name"
+      />
+      <InputComponent
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        type="email"
+        label="email"
+      />
     </>
   );
 };
 
 export default LiftingStateUp;
+
+const InputComponent = ({ inputValue, setInputValue, label, type }) => {
+  return (
+    <>
+      <input
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+        type={type ? type : "text"}
+        placeholder={label}
+      />
+      <h4>insert and show: {inputValue}</h4>
+    </>
+  );
+};
