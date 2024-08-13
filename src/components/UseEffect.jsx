d2b6880a7a844b34045b52219df8237d082ca541
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-14 00:11:30 +0600

    change meta title using use effect and use state hook

--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const UseEffect = () => {
   let [time, setTime] = useState(getTime());
+  let [title, setTitle] = useState("Hello use effect");
 
   function getTime() {
     return new Date().toLocaleTimeString();
@@ -13,10 +14,24 @@ const UseEffect = () => {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
   return (
     <div>
       <h1>Digital clock: Use Effect Hook</h1> <hr />
       <h3>Time: {time}</h3>
+      <div>
+        <label htmlFor="changeMetaTitle">Change meta title: </label>
+        <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          type="text"
+          name="changeMetaTitle"
+          id="changeMetaTitle"
+        />
+      </div>
     </div>
   );
 };
