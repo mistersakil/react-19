f4fbb2fa2b3c057c1497b273006bc9436c294d62
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-08-14 23:57:38 +0600

    fetch data using fetch api, async await, try catch block with proper error handling

--- src/components/FetchApi.jsx
+++ src/components/FetchApi.jsx
@@ -1,20 +1,52 @@
 import React, { useEffect, useState } from "react";
 
 const FetchApi = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
   const [posts, setPosts] = useState([]);
 
+  let [loading, setLoading] = useState(true);
+
+  let [error, setError] = useState(null);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    usingAsyncAwait();
+  }, []);
+
+  function usingThen() {
+    fetch(url)
       .then((response) => response.json())
       .then((json) => setPosts(json));
-  }, []);
+  }
+
+  async function usingAsyncAwait() {
+    try {
+      let fetchData = await fetch(url);
+
+      if (!fetchData.ok) {
+        switch (fetchData.status) {
+          case 404:
+            throw new Error(`HTTP error! 404 - Api not found`);
+        }
+      }
+
+      let data = await fetchData.json();
+
+      setPosts(Array.isArray(data) ? data : []);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  }
 
   return (
     <div>
       <h1>Fetch api</h1>
       <hr />
-
-      {posts.length && (
+      {loading && <h2>loading...</h2>}
+      {error && <h2 style={{ color: "red" }}>{error}</h2>}
+      {posts.length != 0 && (
         <ul>
           {posts.map((item) => (
             <li key={item.id}>{item.title}</li>
